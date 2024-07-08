3a4d263eb5c2e04045cd13cb0bf896f7e22a2b09
Malen28lopez <malopez@asap-consulting.net>
2024-07-08 18:44:57 -0300

    Ejercicio 1 - clase 1

---
+++ src/main/java/clase1/Ejercicio1.java
@@ -0,0 +1,12 @@
+package clase1;
+
+import com.github.sanchezih.util.io.Consola;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+    int numero = Consola.leerEntero("Introduce un numero entero:");
+
+	}
+
+}

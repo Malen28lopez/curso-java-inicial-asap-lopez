b1ef93894be5db6d80896f1fd7b6e0ba7f11f71f
Malen28lopez <malopez@asap-consulting.net>
2024-07-09 20:27:34 -0300

    Ejercicio1 - Clase1

---
+++ src/main/java/ar/edu/ort/p1/util/Consola.java
@@ -0,0 +1,15 @@
+package ar.edu.ort.p1.util;
+
+public class Consola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	static double leerDouble(String string) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- src/main/java/clase1/Ejercicio1.java
+++ src/main/java/clase1/Ejercicio1.java
@@ -3,10 +3,26 @@ package clase1;
 import com.github.sanchezih.util.io.Consola;
 
 public class Ejercicio1 {
+	
+	/**
+	 * Programa que lee 1 numero entero por teclado e indica si es multiplo de 2 o de 3);
+	 */
 
 	public static void main(String[] args) {
-    int numero = Consola.leerEntero("Introduce un numero entero:");
-System.out.println(numero);
-	}
+		int numero = Consola.leerEntero("Introduce un numero entero:");
+		System.out.println(numero);
 
+		if (numero % 2 == 0 || numero % 3 == 0) {
+			if (numero % 2 == 0) {
+				System.out.println("El número es múltipo de 2");
+			}
+			if (numero % 3 == 0) {
+				System.out.println("El número es múltiplo de 3");
+			}
+		} else {
+			System.out.println("El número no es múltiplo de 2 ni múltiplo de 3");
+		}
+	}
 }
+
+
